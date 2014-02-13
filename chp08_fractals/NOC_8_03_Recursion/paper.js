822adaaea54544d47f76394f4b1002d516bcc3d2
dead_horse <dead_horse@qq.com>
2014-02-14 00:58:56 +0800

    update 8.1~8.4

---
+++ chp08_fractals/NOC_8_03_Recursion/paper.js
@@ -0,0 +1,20 @@
+/* global utils */
+(function () {
+  var paper = utils.setup();
+
+  var x = paper.width / 2;
+  var y = paper.height / 2;
+  var r = 200;
+
+  drawCircle(x, y, r);
+
+  function drawCircle(x, y, r) {
+    paper.circle(x, y, r);
+    if (r > 4) {
+      drawCircle(x - r, y, r / 2);
+      drawCircle(x + r, y, r / 2);
+      drawCircle(x, y + r, r / 2);
+      drawCircle(x, y - r, r / 2);
+    }
+  }
+})();
